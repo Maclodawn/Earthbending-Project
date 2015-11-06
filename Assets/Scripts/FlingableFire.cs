ab9b5ca039ae3ee2aec3fee4fe2d5aa76c167e66
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-11-06 18:01:48 -0500

    Fire can be created after 2sec P-key pressed, and when released the fire goes.

---
+++ Assets/Scripts/FlingableFire.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlingableFire : MonoBehaviour {
+
+	public Vector3 Direction;
+	public float Speed;
+
+	private static CreateFire FireCreator = null;
+
+	private bool m_canMoveMyself = false;
+
+	public void Start() {
+		if (FireCreator == null)
+			FireCreator = GameObject.Find("FireManager").GetComponent<CreateFire>();
+	}
+
+	public void Update() {
+		if (m_canMoveMyself) {
+			transform.position += Direction * Time.deltaTime * Speed;
+		} else if (FireCreator.canDetachFire() && !Input.GetKeyDown(KeyCode.P)) {
+			FireCreator.reset();
+			m_canMoveMyself = true;
+		}
+	}
+}
