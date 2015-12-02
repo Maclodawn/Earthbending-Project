cedc012906319e73bc80f2a4e339e2cd169cbf8d
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-12-02 18:25:47 -0500

    Adding frustum management scripts. + scripts (to be continued... :japanese_ogre:)

---
+++ Assets/Scripts/BasicCharacter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicCharacter : MonoBehaviour {
+
+	protected bool m_executingAtk1 = false;
+	protected bool m_executingAtk2 = false;
+	protected bool m_executingAtk3 = false;
+	public GameObject m_attack1Object;
+	public GameObject m_attack2Object;
+	public GameObject m_attack3Object;
+
+	public void init(Manager manager) {
+
+	}
+}
---
+++ Assets/Scripts/Frustum.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Frustum : MonoBehaviour {
+
+	public List<GameObject> objects;
+
+	public void OnTriggerExit(Collider collider) {
+		objects.Remove(collider.gameObject);
+	}
+
+	public void OnTriggerEnter(Collider collider) {
+		objects.Add(collider.gameObject);
+	}
+
+	public List<GameObject> GetObjects() {
+		return objects;
+	}
+}
