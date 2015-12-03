a57758877f6f712628eae4d041af39c63322f879
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-12-03 16:14:01 -0500

    Adding code to spawn attacks + corresponding variables & bug solving: visibility

--- Assets/Scripts/AI/BasicAI.cs
+++ Assets/Scripts/AI/BasicAI.cs
@@ -4,6 +4,25 @@ using System.Collections.Generic;
 
 public class BasicAI : MonoBehaviour {
 
+	public GameObject attack1Object, attack2Object, attack3Object;
+
+	private bool attack1Fire, attack2Fire, attack3Fire;
+
+	public float m_OffsetForwardEarth = 1;
+	private float m_timerAttack = 0.3f;
+	private float m_coolDownAttack = 0.3f;
+	[SerializeField]
+	private float m_rangeToTakeBullet = 5.0f;
+
+	[SerializeField]
+	private float m_attack1ForceUp;
+	[SerializeField]
+	private float m_attack1ForceForward;
+
+	public string m_username = "AIBody";
+
+	//---
+
 	private Frustum[] frustums;
 	private List<GameObject> objects;
 
@@ -43,8 +62,77 @@ public class BasicAI : MonoBehaviour {
 		Vector3 direction = (o.transform.position - transform.position);
 
 		RaycastHit hit;
-		Physics.Raycast(origin + direction.normalized/2, direction, out hit);
+		bool touched = Physics.Raycast(origin + direction.normalized/2, direction, out hit);
+
+		if (!touched) return false;
 
 		return hit.collider.gameObject == o;
+	}
+
+	private void attack1() {
+		Collider[] colliders = Physics.OverlapSphere(transform.position, m_rangeToTakeBullet);
+		FlingableRock bullet = null;
+		
+		if (colliders.Length > 0)
+		{
+			bullet = findBullet(colliders);
+			if (!bullet)
+				spawnAndFlingBullet("Fire1", m_attack1ForceUp, m_attack1ForceForward);
+			else
+			{
+				bullet.setUser(m_username);
+				bullet.fling("Fire1", m_attack1ForceUp, m_attack1ForceForward, false);
+			}
+		}
+		else
+			spawnAndFlingBullet("Fire1", m_attack1ForceUp, m_attack1ForceForward);
+		
+		//m_executingAtk1 = true;
+	}
+
+	private FlingableRock findBullet(Collider[] colliders) {
+		int closerOne = -1;
+		float closerDist = 0;
+		
+		for (int i = 0; i < colliders.Length; ++i)
+		{
+			FlingableRock rock = colliders[i].GetComponent<FlingableRock>();
+			
+			if (rock != null)
+			{
+				if (rock.m_user != null)
+					continue;
+				
+				float distance = Vector3.Distance(transform.position, rock.transform.position);
+				if (distance < m_rangeToTakeBullet && (closerOne == -1 || closerDist > distance))
+				{
+					closerDist = distance;
+					closerOne = i;
+				}
+			}
+		}
+		
+		if (closerOne == -1)
+			return null;
+		else
+			return colliders[closerOne].GetComponent<FlingableRock>();
+	}
+
+	private void spawnAndFlingBullet(string _buttonToWatch, float _forceUp, float _forceForward) {
+		Vector3 spawnProjectile = transform.position + transform.forward * m_OffsetForwardEarth;
+		RaycastHit hit;
+		if (Physics.Raycast(spawnProjectile, -Vector3.up, out hit, 50))
+		{
+			if (attack1Object.transform.childCount == 0)
+				return;
+
+			Transform child = attack1Object.transform.GetChild(0);
+			MeshRenderer meshRenderer = child.GetComponent<MeshRenderer>();
+			spawnProjectile = hit.point - new Vector3(0, meshRenderer.bounds.extents.y, 0);
+			
+			FlingableRock tmpBullet = ((GameObject)Instantiate(attack1Object, spawnProjectile, Quaternion.identity)).GetComponent<FlingableRock>();
+			tmpBullet.setUser(m_username);
+			tmpBullet.init(_buttonToWatch, _forceUp, _forceForward);
+		}
 	}
 }
