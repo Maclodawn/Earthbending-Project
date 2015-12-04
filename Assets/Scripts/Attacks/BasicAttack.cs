aeb5e978cbaa3622bf59e09f3e203c583aa15a14
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-12-03 19:59:14 -0500

    BasicAttack (component model for attacks)

---
+++ Assets/Scripts/Attacks/BasicAttack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class BasicAttack : MonoBehaviour {
+
+	private bool icanexecute;
+	private CharacterMovement executer;
+	
+	public void Start() {
+		executer = GetComponent<CharacterMovementEarth>();
+	}
+
+	public void Update() {
+		if (!icanexecute)
+			return;
+
+		updateMe();
+
+		icanexecute = false;
+	}
+
+	public void executeAttack() {
+		icanexecute = true;
+	}
+
+	public abstract void updateMe();
+}
