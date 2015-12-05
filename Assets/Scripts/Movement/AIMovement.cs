0618f6079d889198e3af8a254954f03ef54ca0e4
belkalah-mbp <belkacem.lahouel@gmail.com>
2015-12-04 19:46:17 -0500

    AIMovement added: basic wander for now.

---
+++ Assets/Scripts/Movement/AIMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovement : BasicMovement {
+
+	protected override void updateInput() {
+		float degrees = Random.Range(-15f, 15f);
+		transform.Rotate(0f, degrees, 0f);
+		input.x = -Mathf.Sin(degrees*Mathf.Deg2Rad);
+		input.y = Mathf.Cos(degrees*Mathf.Deg2Rad);
+
+		//Debug.DrawLine(transform.position, transform.position + transform.forward);
+
+		jump = (Mathf.Abs(degrees) > 59.5f) ? true : false;
+		sprint = (Mathf.Abs(degrees) > 55 && Mathf.Abs(degrees) < 57) ? true : false;
+	}
+}
