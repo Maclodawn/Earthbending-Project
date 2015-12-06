70174315f56de327ac9ec84954f7d52ccbcb9ddd
Donatien Rabiller <rabiller.donatien@hotmail.fr>
2015-12-05 22:39:25 -0500

    Fix a weird bug, need to confirm.

--- Assets/Scripts/Attacks/AttackLaunchers/AIAttackLauncher.cs
+++ Assets/Scripts/Attacks/AttackLaunchers/AIAttackLauncher.cs
@@ -2,16 +2,26 @@
 using System.Collections;
 
 public class AIAttackLauncher : AttackLauncher {
-	
+
+    private float m_abortTimer = 0;
+
 	protected override void updateInput() {
 		//TODO AI thinking here
 		atk = 0; //(Random.Range(0, 11) > 9) ? 0 : -1;
 
-		if (hold) {
-			bool tmp = atks[0].isFinished();
-			if (tmp)
-				hold = false;
-		} else if (atk == 0)
-			hold = true;
+        bool atkFinished = atks[0].isFinished();
+
+        if (hold == true) // we increase the abort timer while we're holding an attack
+            m_abortTimer += Time.deltaTime;
+
+        if (hold == true && (m_abortTimer >= 2.0f || atkFinished)) // Else we abort or the attack is finished
+        {
+            hold = false;
+            m_abortTimer = 0;
+		} 
+        else if (atk == 0)
+        {
+            hold = true;
+        }
 	}
 }
